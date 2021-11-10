faa9e47c2c2518d758498ccc84800a4e2c427b97
Nathan May <nathaniel.may22@gmail.com>
2021-11-10 15:12:37 -0500

    app created bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
+  const log = (value: boolean) => console.log(value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <a href="">GitHub Repository</a>
+      <h1>Rhyme Finder (579 Problem Set 5)</h1>
+      <Button onClick={() => log(true)}>Show Toast</Button>
+    </main>
   );
 }
 
